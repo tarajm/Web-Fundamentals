48e735a53cedcb6c08bae4d983652d251f847046
Tara J. Massan <70990388+tarajm@users.noreply.github.com>
2022-12-05 18:42:06 -0600

    Delete swap images directory

--- swap images/script.js
+++
@@ -1,9 +0,0 @@
-console.log("hello there!")
-
-function swap() {
-    var Walter = document.getElementById("#walter");
-        White = document.getElementById("#snow");
-        Water = document.getElementById("#waterboy");
-    Water.src = Walter;
-    Water.src = White;    
-}
